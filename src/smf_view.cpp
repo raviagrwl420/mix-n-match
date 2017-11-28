b349f472a0ffdefa0234a971ee760b0e7bf640e3
Ravi Agrawal <ravi.agrwl420@gmail.com>
2017-11-28 12:06:25 -0800

    Cleanup smf_view.cpp

--- src/smf_view.cpp
+++ src/smf_view.cpp
@@ -7,13 +7,14 @@
 	@brief		CMPT-764: Assignment 2, Implements SMF viewer.
 */
 
-#include <smf_parser.h>
 #include <GL/glut.h>
 #include <GL/glui.h>
 #include <memory>
 #include <stdexcept>
 #include <string>
 #include <array>
+
+#include <part.h>
 
 #define WIDTH 1200
 #define HEIGHT 800
@@ -22,10 +23,9 @@ using std::runtime_error;
 using std::shared_ptr;
 using std::array;
 
-enum DisplayType {FLAT_SHADED, SMOOTH_SHADED, WIREFRAME, SHADED_WITH_EDGES};
-enum Buttons {ROTATION, OPEN, SAVE, QUIT, SUBDIVIDE, DECIMATE};
+enum Buttons {ROTATION, OPEN, SAVE, QUIT};
 
-Mesh *mesh;
+Part part;
 
 float xy_aspect;
 int last_x, last_y;
@@ -37,14 +37,6 @@ int displayType = FLAT_SHADED;
 float scale = 1.0;
 float view_rotate[16] = {1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1};
 float obj_pos[] = {0.0, 0.0, 0.0};
-
-// Subdivision
-int subdivisionType = BUTTERFLY;
-int subdivisionLevel = 1;
-
-// Decimation
-int decimationK = 1;
-int decimationNumber = 1;
 
 // GLUT idle function
 void glutIdle (void) {
@@ -62,111 +54,9 @@ void glutReshape (int x, int y) {
 	glutPostRedisplay();
 }
 
-// Displays mesh as flat shaded
-void displayFlatShaded (void) {
-	glShadeModel(GL_FLAT);
-	glEnable(GL_NORMALIZE);
-	glBegin(GL_TRIANGLES);
-	
-	Face *f;
-	Vertex *vertices[3];
-
-	vec3 faceNormal;
-
-	for (int i = 1; i <= mesh->numFaces; i++) {
-		f = mesh->faceMap[i];
-
-		faceNormal = mesh->getFaceNormal(f);
-		glNormal3f(faceNormal.x, faceNormal.y, faceNormal.z);
-
-		mesh->getAllVerticesForFace(f, vertices);
-
-		for (int j = 0; j < 3; j++) {
-			vec3 position = vertices[j]->position;
-			glVertex3f(position.x, position.y, position.z);
-		}
-	}
-
-	glEnd();
-}
-
-// Displays mesh as smooth shaded
-void displaySmoothShaded (void) {
-	glShadeModel(GL_SMOOTH);
-	glEnable(GL_NORMALIZE);
-	glBegin(GL_TRIANGLES);
-	
-	Face *f;
-	Vertex *vertices[3];
-	vec3 vertexNormal;
-
-	for (int i = 1; i <= mesh->numFaces; i++) {
-		f = mesh->faceMap[i];
-
-		mesh->getAllVerticesForFace(f, vertices);
-
-		for (int j = 0; j < 3; j++) {
-			vertexNormal = vertices[j]->normal;
-			glNormal3f(vertexNormal.x, vertexNormal.y, vertexNormal.z);
-
-			vec3 position = vertices[j]->position;
-			glVertex3f(position.x, position.y, position.z);
-		}
-	}
-
-	glEnd();
-}
-
-// Display mesh as wireframe
-void displayWireframe (void) {
-	glBegin(GL_LINES);
-
-	W_edge *edge;
-
-	for (map<string, W_edge*>::const_iterator it = mesh->edgeMap.begin(); it != mesh->edgeMap.end(); it++) {
-		edge = it->second;
-
-		if (edge == NULL) {
-			std::cout << "First!!::" << it->first << std::endl;
-			std::cout << "edge:" << edge << std::endl;
-			std::cout << "edge index:" << mesh->edgeIndexMap[edge] << std::endl;
-			std::cout << "edge key:" << mesh->edgeKeyMap[mesh->edgeIndexMap[edge]] << std::endl;
-		}
-		
-
-		vec3 startPosition = edge->start->position;
-		vec3 endPosition = edge->end->position;
-
-		glVertex3f(startPosition.x, startPosition.y, startPosition.z);
-		glVertex3f(endPosition.x, endPosition.y, endPosition.z);
-
-	}
-
-	glEnd();
-}
-
 // Display mesh function
 void displayMesh (void) {
-	if (mesh == NULL)
-		return;
-
-	switch (displayType) {
-		case FLAT_SHADED:
-			displayFlatShaded();
-			break;
-		case SMOOTH_SHADED:
-			displaySmoothShaded();
-			break;
-		case WIREFRAME:
-			displayWireframe();
-			break;
-		case SHADED_WITH_EDGES:
-			displayFlatShaded();
-			glEnable(GL_POLYGON_OFFSET_FILL);
-			glPolygonOffset(1.0, 1.0);
-			displayWireframe();
-			break;
-	}
+	part.render((DisplayType) displayType);
 }
 
 // GLUT display function
@@ -206,15 +96,6 @@ string exec(const char* cmd) {
     return result;
 }
 
-// Initialize mesh
-void initMesh (string smf_filename) {
-	mesh = parseSmfFile(smf_filename);
-	mesh->computeBoundingBox();
-	obj_pos[0] = -(mesh->xMin + mesh->xMax) / 2;
-	obj_pos[1] = -(mesh->yMin + mesh->yMax) / 2;
-	obj_pos[2] = 1.5 * (mesh->zMin - mesh->zMax);
-}
-
 // GLUI control callback
 void control_cb(int control) {
 	switch (control) {
@@ -224,7 +105,7 @@ void control_cb(int control) {
 			// Remove the newline character at the end
 			inputFilePath = inputFilePath.substr(0, inputFilePath.size() - 1);
 			if (inputFilePath.size() != 0)
-				initMesh(inputFilePath);
+				part = Part::initPart(inputFilePath);
 			break;
 		}
 
@@ -234,25 +115,12 @@ void control_cb(int control) {
 			// Remove the newline character at the end
 			saveFilePath = saveFilePath.substr(0, saveFilePath.size() - 1);
 			if (saveFilePath.size() != 0)
-				writeSmfFile(mesh, saveFilePath);
+				part.writePart(saveFilePath);
 			break;
 		}
 	}
 };
 
-void subdivision_cb (int control) {
-	if (mesh == NULL)
-		return;
-
-	mesh = mesh->subdivideMesh(subdivisionType, subdivisionLevel);
-}
-
-void decimation_cb (int control) {
-	if (mesh == NULL)
-		return;
-
-	mesh->decimate(decimationK, decimationNumber);
-}
 
 // Setup GLUI
 void setupGlui () {
@@ -265,8 +133,6 @@ void setupGlui () {
 	// Setup UI
 	// Add Panel "Display Options"
 	GLUI_Panel *displayOptionsPanel = glui->add_panel("Display Options");
-	GLUI_Panel *subdivisionPanel = glui->add_panel("Subdivision");
-	GLUI_Panel *decimationPanel = glui->add_panel("Decimation");
 	GLUI_Panel *transformationsPanel = glui->add_panel("Transformations");
 	GLUI_Panel *controlsPanel = glui->add_panel("Controls");
 
@@ -276,29 +142,6 @@ void setupGlui () {
 	listbox->add_item(SMOOTH_SHADED, "Smooth Shaded");
 	listbox->add_item(WIREFRAME, "Wireframe");
 	listbox->add_item(SHADED_WITH_EDGES, "Shaded with Edges");
-
-	// Subdivision
-	GLUI_Listbox *subdivisionListbox = new GLUI_Listbox(subdivisionPanel, "Type:", &subdivisionType);
-	subdivisionListbox->add_item(BUTTERFLY, "Butterfly");
-	subdivisionListbox->add_item(LOOP, "Loop");
-	subdivisionListbox->set_alignment(GLUI_ALIGN_RIGHT);
-
-	GLUI_Spinner *subdivision_level_spinner = new GLUI_Spinner(subdivisionPanel, "Level:", &subdivisionLevel);
-  	subdivision_level_spinner->set_int_limits(1, 5);
-  	subdivision_level_spinner->set_alignment(GLUI_ALIGN_RIGHT);
-
-  	glui->add_button_to_panel(subdivisionPanel, "subdivide", SUBDIVIDE, subdivision_cb);
-
-  	// Decimation
-	GLUI_Spinner *decimation_k_spinner = new GLUI_Spinner(decimationPanel, "k:", &decimationK);
-	decimation_k_spinner->set_int_limits(1, 20000);
-	decimation_k_spinner->set_alignment(GLUI_ALIGN_RIGHT);
-
-	GLUI_Spinner *decimation_number_spinner = new GLUI_Spinner(decimationPanel, "Edge Count:", &decimationNumber);
-	decimation_number_spinner->set_int_limits(1, 20000);
-	decimation_number_spinner->set_alignment(GLUI_ALIGN_RIGHT);
-
-	glui->add_button_to_panel(decimationPanel, "decimate", DECIMATE, decimation_cb);
 
 	// Add Scale Spinner
 	GLUI_Spinner *scale_spinner = new GLUI_Spinner(transformationsPanel, "Scale:", &scale);
