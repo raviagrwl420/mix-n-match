253fe014841f26d8eda5bbe22c3b104313b35431
Ravi Agrawal <ravi.agrwl420@gmail.com>
2017-12-02 20:21:41 -0800

    Add method to get center of bounding box

--- include/part_base.h
+++ include/part_base.h
@@ -26,6 +26,7 @@ public:
 
 	virtual void render (DisplayType displayType) {};
 	void renderBoundingBox ();
+	Vector getCenter ();
 };
 
 void PartBase::renderBoundingBox () {
@@ -68,6 +69,13 @@ void PartBase::renderBoundingBox () {
 		glVertex3f(boundingBox.xmin(), boundingBox.ymax(), boundingBox.zmax());
 		glVertex3f(boundingBox.xmin(), boundingBox.ymin(), boundingBox.zmax());
 	glEnd();
+}
+
+Vector PartBase::getCenter () {
+	double x = (boundingBox.xmin() + boundingBox.xmax())/2;
+	double y = (boundingBox.ymin() + boundingBox.ymax())/2;
+	double z = (boundingBox.zmin() + boundingBox.zmax())/2;
+	return Vector(x, y, z);
 }
 
 #endif
