84616f7f988c93ce7b113c73724639371bf9b2bc
Ravi Agrawal <ravi.agrwl420@gmail.com>
2017-12-10 00:58:22 -0800

    Add method to get minimum distance between two mesh

--- include/cgal.h
+++ include/cgal.h
@@ -16,6 +16,22 @@
 #include <CGAL/Polygon_mesh_processing/compute_normal.h>
 #include <CGAL/Polygon_mesh_processing/bbox.h>
 
+// Optimal Distances Package
+#include <CGAL/Polytope_distance_d.h>
+#include <CGAL/Polytope_distance_d_traits_3.h>
+#include <CGAL/Homogeneous.h>
+
+#ifdef CGAL_USE_GMP
+#include <CGAL/Gmpzf.h>
+typedef CGAL::Gmpzf ET;
+#else
+#include <CGAL/MP_Float.h>
+typedef CGAL::MP_Float ET;
+#endif
+
+#include <iostream>
+#include <math.h>
+
 typedef CGAL::Simple_cartesian<double> K;
 typedef K::Point_3 Point;
 typedef K::Vector_3 Vector;
@@ -24,6 +40,10 @@ typedef Mesh::Vertex_index VertexIndex;
 typedef Mesh::Face_index FaceIndex;
 typedef Mesh::Edge_index EdgeIndex;
 typedef CGAL::Bbox_3 BoundingBox;
+
+// Optimal Distances Package
+typedef CGAL::Polytope_distance_d_traits_3<K, ET, double> Traits;
+typedef CGAL::Polytope_distance_d<Traits> Polytope_distance;
 
 double getScale (BoundingBox boundingBox) {
 	double xRange = boundingBox.xmax() - boundingBox.xmin();
@@ -42,5 +62,11 @@ Vector getCenter (BoundingBox boundingBox) {
 	double z = (boundingBox.zmin() + boundingBox.zmax())/2;
 	return Vector(x, y, z);
 };
+
+double getMinimumDistance (Mesh mesh1, Mesh mesh2) {
+	Polytope_distance pd(mesh1.points().begin(), mesh1.points().end(), mesh2.points().begin(), mesh2.points().end());
+
+	return sqrt(CGAL::to_double (pd.squared_distance_numerator()) / CGAL::to_double (pd.squared_distance_denominator()));
+}
 
 #endif
